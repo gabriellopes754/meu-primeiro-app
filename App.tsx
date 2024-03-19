64a9cabd51642f16e4c5af9b9f6df681f13abe0d
gabriellopes754 <gabriellopes15942@outlook.com>
2024-03-18 22:14:52 -0300

    atualização média

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
 
 export default function App() {
+  const [novaVariavel, setNovaVariavel] = React.useState('');
+  const [novaVar, setNovaVar] = React.useState('');
+  const calculo = (parseFloat(novaVariavel) + parseFloat(novaVar)) / 2;
   return (
     <View style={styles.container}>
-      <Text style={styles.field}>nome:</Text>
-      <TextInput style={styles.textinho} />
+      <Text style={styles.field}>numero 1:</Text>
+      <TextInput keyboardType='numeric' onChangeText = {setNovaVariavel} style={styles.textinho} />
+      
+
+      <Text style={styles.field}>numero 2:</Text>
+      <TextInput keyboardType='numeric' onChangeText = {setNovaVar} style={styles.textinho} />
+      {!isNaN(calculo) && <Text style={styles.field}>{calculo}</Text>}
+
+      <TouchableOpacity style={Text.button}>
+        <Text style={styles.field}>enviar</Text>  
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -30,5 +44,13 @@ const styles = StyleSheet.create({
     borderRadius:10,
     padding:10,
     marginTop:10,
+  },
+  button: {
+    backgroundColor: 'rgb(54,54,54)',
+    width: 'auto',
+    borderRadius:10,
+    padding:10,
+    marginTop:10,
+    alignItems: 'center'
   }
 });
